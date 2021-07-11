d85bb2c8b36d23892e825d370d6f9af73a4f108a
Kyle Davis <kpd328@users.noreply.github.com>
2021-07-11 16:47:28 -0700

    moved logging to the _dev-mode module

--- module/scripts/rosetta.js
+++ module/scripts/rosetta.js
@@ -1,5 +1,17 @@
-function log(message){
-    console.log('Rosetta | ' + message);
+class Rosetta {
+    static ID = 'rosetta';
+
+    static log(force, ...args) {
+        const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
+
+        if (shouldLog) {
+            console.log(this.ID, '|', ...args);
+        }
+    }
 }
 
-log('Starting module');
+Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
+    registerPackageDebugFlag(ToDoList.ID);
+});
+
+log(false, 'Starting Module.');
