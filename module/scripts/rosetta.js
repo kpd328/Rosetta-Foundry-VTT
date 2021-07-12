517e376b6452b2f5522b23c41baf4dda01947213
Kyle Davis <kpd328@users.noreply.github.com>
2021-07-11 17:08:47 -0700

    documentation and frameworking

--- module/scripts/rosetta.js
+++ module/scripts/rosetta.js
@@ -3,12 +3,28 @@
  */
 class Rosetta {
     static ID = 'rosetta';
+
+    static FLAGS = {
+
+    }
+
+    static TEMPLATES = {
+        
+    }
 }
 
+/**
+ * Register debug flag with developer mode's custom hook
+ */
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
     registerPackageDebugFlag(Rosetta.ID);
 });
 
+/**
+ * A small helper function which leverages developer mode flags to gate debug logs.
+ * @param {boolean} force - forces the log even if the debug flag is not on
+ * @param  {...any} args - what to log
+ */
 function log(force, ...args) {
     try{
         const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(Rosetta.ID);
